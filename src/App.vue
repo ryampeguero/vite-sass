f9f79549b75a4d0855b101436c21ba26e62af386
ryampeguero <ryampeguero@gmail.com>
2024-05-02 10:24:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppBanner from "./components/AppBanner.vue";
+
+export default {
+  components: {
+    AppBanner
+  },
+
+  data(){
+    return {
+      title: "ciao"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <AppBanner />
+</template>
+
+<style>
+</style>
---
+++ src/components/AppBanner.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="banner">
+    <h2>Titolo</h2>
+  </div>
+</template>
+
+
+<style>
+.banner {
+  height: 60vh;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
